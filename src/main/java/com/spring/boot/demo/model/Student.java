9637c4e8094212c7400bfb772fa78c93fd3de856
Rabia <sdetcode@gmail.com>
2022-01-04 21:48:38 +0530

    spring data jpa integration

--- src/main/java/com/spring/boot/demo/model/Student.java
+++ src/main/java/com/spring/boot/demo/model/Student.java
@@ -4,13 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
