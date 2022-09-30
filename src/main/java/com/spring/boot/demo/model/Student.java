c3d18c17ba6dbe71f1a9cfe34f20b622d107a8b3
Rabia <sdetcode@gmail.com>
2022-09-30 23:34:02 +0530

    find student by emailId,Id

--- src/main/java/com/spring/boot/demo/model/Student.java
+++ src/main/java/com/spring/boot/demo/model/Student.java
@@ -1,6 +1,7 @@
 package com.spring.boot.demo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Entity
 @Table
+@Builder
 public class Student {
 
     @Id
